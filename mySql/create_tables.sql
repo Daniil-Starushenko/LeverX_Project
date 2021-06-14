c2a20d4e747ececfd948a9034b46ecb898b8dd53
Daniil-Starushenko <daniil80801@gmail.com>
2021-06-15 00:03:32 +0300

    complete sql scripts

--- mySql/create_tables.sql
+++ mySql/create_tables.sql
@@ -5,14 +5,37 @@ create table `user` (
                         `last_name` VARCHAR(255) NOT NULL,
                         `password` VARCHAR(255) NOT NULL,
                         `email` varchar(255) NOT NULL,
-                        `registration_date` DATE,
+                        `registration_date` DATE NOT NULL ,
+
                         PRIMARY KEY(`id`),
                         UNIQUE (id)
 );
 
 create table `article` (
-                        `id` INTEGER NOT NULL AUTO_INCREMENT,
+                           `id` INTEGER NOT NULL AUTO_INCREMENT,
+                           `title` VARCHAR(255) NOT NULL,
+                           `text` TEXT NOT NULL,
+                           `status` ENUM('PRIVATE', 'DRAFT'),
+                           `author_id` INTEGER,
+                           `created_at` DATE NOT NULL,
+                           `changed_at` DATE NOT NULL,
 
-                        PRIMARY KEY(`id`),
-                        UNIQUE (id)
+                           PRIMARY KEY(`id`),
+                           FOREIGN KEY (`author_id`) REFERENCES `user`(`id`),
+                           UNIQUE (id)
+);
+
+create table `article_unit` (
+                            `author_id` INTEGER,
+                            `article_id` INTEGER,
+
+                            FOREIGN KEY (`author_id`) REFERENCES `user`(`id`),
+                            FOREIGN KEY (`article_id`) REFERENCES `article`(`id`)
+);
+
+create table `tag` (
+                       `article_id` INTEGER,
+                       `value` ENUM('PROGRAMMING', 'LIFESTYLE', 'JOB', 'HOME'),
+
+                       FOREIGN KEY (`article_id`) REFERENCES `article`(`id`)
 );
